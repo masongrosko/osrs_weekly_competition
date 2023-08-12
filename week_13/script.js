d080c978114b1b6cd3c151b93b449b6363ea2045
masongrosko <69281238+masongrosko@users.noreply.github.com>
2023-08-12 14:47:00 -0500

    Create script.js

---
+++ week_13/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const xpInput = document.getElementById("xpInput");
+    const wonCompetitionCheckbox = document.getElementById("wonCompetition");
+    const calculateButton = document.getElementById("calculateButton");
+    const resultsDiv = document.getElementById("results");
+
+    calculateButton.addEventListener("click", function() {
+        const xpGained = parseInt(xpInput.value);
+        const wonCompetition = wonCompetitionCheckbox.checked;
+
+        const thousands = Math.floor(xpGained / 1000);
+        const hundredThousands = Math.floor(xpGained / 100000);
+
+        const karams = thousands + 100 * hundredThousands;
+        if (wonCompetition) {
+            karams += 1000;
+        }
+        const cBalls = 10 * karams;
+
+        const resultText = `You would receive ${karams} karambwans and ${cBalls} cannon balls!`;
+        resultsDiv.textContent = resultText;
+    });
+});
