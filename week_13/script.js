b4e74b90a9152a0663502e207b7c721ede2ed957
masongrosko <69281238+masongrosko@users.noreply.github.com>
2023-08-13 10:48:45 -0500

    Update script.js

--- week_13/script.js
+++ week_13/script.js
@@ -1,67 +1,32 @@
+const karambwanImage = '<img src="https://oldschool.runescape.wiki/images/thumb/Cooked_karambwan_detail.png/1024px-Cooked_karambwan_detail.png?43b37" alt="Karambwan Image" style="height: 2em; vertical-align: middle;">';
+const cannonballImage = '<img src="https://oldschool.runescape.wiki/images/thumb/Cannonball_detail.png/1024px-Cannonball_detail.png?cbad8" alt="Cannonball Image" style="height: 2em; vertical-align: middle;">';
+const competitionNumber = 28879;
+const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Fetch data from the API
-    fetch("https://api.wiseoldman.net/v2/competitions/28879")
-        .then(response => response.json())
-        .then(data => {
-            const participants = data.participations;
-
-            // Create and populate the table
-            const table = document.createElement("table");
-            table.classList.add("participants");
-
-            participants.forEach(participant => {
-                const row = table.insertRow();
-                const nameCell = row.insertCell();
-                const progressCell = row.insertCell();
-
-                nameCell.textContent = participant.player.displayName;
-                progressCell.textContent = participant.progress.gained;
-
-                // Append row to the table
-                table.appendChild(row);
-            });
-
-            // Append table to the participantsTable div
-            participantsTable.appendChild(table);
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        });
-
     const xpInput = document.getElementById("xpInput");
     const wonCompetitionCheckbox = document.getElementById("wonCompetition");
     const calculateButton = document.getElementById("calculateButton");
     const resultsDiv = document.getElementById("results");
-
-    // Format number with commas for thousands
-    function formatNumberWithCommas(number) {
-        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+    const participantsTable = document.getElementById("participantsTable");
+    const reloadTableButton = document.getElementById("reloadTableButton");
 
     // Format input value with commas while typing
     xpInput.addEventListener("input", function() {
-        const currentValue = xpInput.value.replace(/,/g, ''); // Remove existing commas
-        const numericValue = currentValue.replace(/[^\d]/g, ''); // Remove non-numeric characters
-        const formattedValue = formatNumberWithCommas(numericValue);
-        xpInput.value = formattedValue;
+        xpInput.value = formatNumberWithCommas(sanitizeInput(xpInput.value));
     });
 
+    // Create calulation when calulate button clicked
     calculateButton.addEventListener("click", function() {
-        const xpGained = parseInt(xpInput.value.replace(/,/g, '')); // Remove commas for calculation
+        const xpGained = parseInt(sanitizeInput(xpInput.value)); // Convert to int
         const wonCompetition = wonCompetitionCheckbox.checked;
 
         const thousands = Math.floor(xpGained / 1000);
         const hundredThousands = Math.floor(xpGained / 100000);
 
-        let karams = thousands + 100 * hundredThousands;
-        if (wonCompetition) {
-            karams += 1000; // Add 1000 only if the checkbox is checked
-        }
-        const cBalls = 10 * karams;
+        const karams = calculateKarambwans(xpGained, wonCompetition)
+        const cBalls = calculateCannonballs(xpGained, wonCompetition)
 
-        const formattedXP = formatNumberWithCommas(xpGained);
-        const karambwanImage = '<img src="https://oldschool.runescape.wiki/images/thumb/Cooked_karambwan_detail.png/1024px-Cooked_karambwan_detail.png?43b37" alt="Karambwan Image" style="height: 2em; vertical-align: middle;">';
-        const cannonballImage = '<img src="https://oldschool.runescape.wiki/images/thumb/Cannonball_detail.png/1024px-Cannonball_detail.png?cbad8" alt="Cannonball Image" style="height: 2em; vertical-align: middle;">';
         const resultText = `<br>${karambwanImage} ${formatNumberWithCommas(karams)} karambwans<br>${cannonballImage} ${formatNumberWithCommas(cBalls)} cannon balls`;
         resultsDiv.innerHTML = resultText;
     });
@@ -74,4 +39,98 @@ document.addEventListener("DOMContentLoaded", function() {
     // Clear results when input is changed
     xpInput.addEventListener("input", clearResults);
     wonCompetitionCheckbox.addEventListener("change", clearResults);
+
+    // Populate table from wiseoldman
+    participantsTable.appendChild(fetchAndUpdateTable(competitionNumber));
+    
+    // Reload table on request
+    reloadTableButton.addEventListener("click", function() {
+        participantsTable.innerHTML = "";  // Clear previous table
+        participantsTable.appendChild(fetchAndUpdateTable(competitionNumber));
+    });
 });
+
+
+// Sanitize input in realtime
+function sanitizeInput(inputValue) {
+    return (
+        inputValue
+        .replace(/,/g, '')  // Remove existing commas
+        .replace(/[^\d]/g, '')  // Remove non-numeric characters
+    );
+}
+
+
+// Format number with commas for thousands
+function formatNumberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+// Fetch wiseoldman data
+function fetchWiseOldManAPI(endpoint) {
+    return fetch(`https://api.wiseoldman.net/v2${endpoint}`)
+        .then(response => response.json())
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+}
+
+
+// Fetch and update table
+function fetchAndUpdateTable(competitionNumber) {
+    const endpoint = `/competitions/${competitionNumber}`;
+    const table = document.createElement("table");
+    fetchWiseOldManAPI(endpoint).then(
+        data => {
+            const participants = data.participations;
+
+            // Populate the table
+            table.classList.add("participants");
+
+            participants.forEach(participant => {
+                const row = table.insertRow();
+                const nameCell = row.insertCell();
+                const xpCell = row.insertCell();
+                const karambwansCell = row.insertCell();
+                const cannonballsCell = row.insertCell();
+                
+                const participantName = participant.player.displayName;
+                const xpGained = participant.progress.gained;
+                const karams = calculateKarambwans(xpGained);
+                const cBalls = calculateCannonballs(xpGained);
+
+                nameCell.textContent = participantName;
+                xpCell.textContent = formatNumberWithCommas(xpGained);
+                karambwansCell.innerHTML = `${karambwanImage} ${formatNumberWithCommas(karams)}`;
+                cannonballsCell.innerHTML = `${cannonballImage} ${formatNumberWithCommas(cBalls)}`;
+
+                // Append row to the table
+                table.appendChild(row);
+            });
+        }
+    )
+    return table;
+}
+
+// Function to calculate karambwans
+function calculateKarambwans(xpGained, wonCompetition) {
+    const thousands = Math.floor(xpGained / 1000);
+    const hundredThousands = Math.floor(xpGained / 100000);
+
+    let karams = thousands + 100 * hundredThousands;
+
+    if (wonCompetition) {
+        karams += 1000; // Add 1000 only if the checkbox is checked
+    }
+
+    return karams;
+}
+
+// Function to calculate cannonballs
+function calculateCannonballs(xpGained, wonCompetition) {
+    const karams = calculateKarambwans(xpGained, wonCompetition);
+    const cannonballs = 10 * karams;
+
+    return cannonballs;
+}
